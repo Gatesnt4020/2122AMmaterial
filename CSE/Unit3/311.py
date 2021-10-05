a5b460063f94f4cc75b33df1176111ed549bfbf9
younglings <christian.gates@evsck12.com>
2021-10-05 09:19:19 -0500

    initial push

---
+++ CSE/Unit3/311.py
@@ -0,0 +1,37 @@
+print("hello world")
+
+# this is to start a single line comment 
+'''
+    ToDo
+        check different data types
+        do some math
+        do input and output
+'''
+
+''' VERBS
+    Functions/Methods/Procedures
+        function - a list of steps (algorithm) that definded to a word or phrase
+        methods - OOP -object.methods or functions that objects run
+        procedure - purple block of MIT
+'''
+
+#NOUNS = variables or objects
+
+#call TheFunctionPrint("message to print")
+print("add somthing else")
+print("bonjour")
+print("hola")
+print("priviet")
+print("konichiwa")
+print(123456789)
+print("123456789")
+#no no on renaming or using functions as variables
+print = "something"
+input = input("message to the user")
+
+#variable naming sytnax
+camelCase = "a;sdfj;asdfj;"
+#under_score_case = "asd;jfkjasdf;lj"
+NAMEOFVARIABLE = "this represents a CONSTANT"
+PI = 3.14
+NewYear = "January 1st"
---
+++ CSE/Unit3/312.py
@@ -0,0 +1,38 @@
+#output a statement
+print()
+#input something
+input()
+
+#save the info from the user to a variable 
+name = input("what is your name?")
+
+#math
+x=0
+#add 10 to x 
+x = x+10
+#subtract 5 to x
+x= x-5
+#multiply x by 2
+x = x*2
+# divide x by 5
+x = x/5
+
+print(x)
+
+x=0
+#add 10 to x 
+x+=10
+#subtract 5 to x
+x-=5
+#multiply x by 2
+x*=2
+# divide x by 5
+x/=5
+
+print(x)
+
+#exponents
+x=x*x
+x=x**2
+#x=x^2 this is not the exponent like on calc
+print(x)
