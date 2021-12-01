d78815cb8ab60972aca6139bf0b085070596603c
younglings <christian.gates@evsck12.com>
2021-12-01 07:27:59 -0600

    ah general kenobi

--- CSE/Unit3/Notes/CourseRegistration/Main.py
+++ CSE/Unit3/Notes/CourseRegistration/Main.py
@@ -2,31 +2,49 @@ from Course import Course
 from Student import Student
 from Teacher import Teacher
 #from Assignment import * #literally imports all functions and classes
-teacherList = [Teacher("Banderino","ScienceInSpanish"),Teacher("Anderson","Math"),Teacher("Alexander","Science")]
-courseList = [Course("Lunch",teacherList[0]),Course("Alegbra",teacherList[1]),Course("Anatomy",teacherList[2])]
+teacherList=[]
+courseList = []
 studentList=[]
 
-first=""
-last=""
-newStudent="y"
-#keep looping while you have a newStudent
-while newStudent == "n":
-    first=input("Student's First: ")
-    last=input("Student's Last: ")
-    studentList.append(Student(first,last))
+#loadin the teacgers
+with open("Data/teacher.csv","r") as file:
+    #readlines returns a list
+    teacherFile= file.readlines()
+
+for line in teacherFile:
+    name,course,junk = line.split("\t",2)
     
-    newStudent = input("Do you have a new student? (y/n)")
-
-for student in studentList:    
-    print(studentList)
+    #create a teacher object
+    teacherList.append(Teacher(name,course))
+    #save the teachers in the teacherList
     
+#delete the headers
+teacherList.pop(0)
 
-classToLookUp = "Algebra"
-for course in courseList:
-    if course.name == classToLookUp:
-        print(course.courseId, course.name, course.teacher.name)
+i=-1
+for line in teacherFile:
+    nameOfTeacher,nameOfCourse,junk = line.split("\t",2)
+    if i!= -1:
+        courseList.append(Course(nameOfCourse,teacherList[i]))
+    i+=1
+print(courseList[0].name)
 
 
+def loadStudent():
+    newStudent="y"
+    #keep looping while you have a newStudent
+    while newStudent == "n":
+        first=input("Student's First: ")
+        last=input("Student's Last: ")
+        studentList.append(Student(first,last))
+        
+        newStudent = input("Do you have a new student? (y/n)")
 
-
-
+    for student in studentList:    
+        print(studentList)
+    
+def courseLookup():
+    classToLookUp = "Algebra"
+    for course in courseList:
+        if course.name == classToLookUp:
+            print(course.courseId, course.name, course.teacher.name)
