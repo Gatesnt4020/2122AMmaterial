0865dad7309901a1dd1eec051d29bb6a48da135b
younglings <christian.gates@evsck12.com>
2021-12-07 09:40:18 -0600

    filewriteing '

--- CSE/Unit3/Notes/FileWriting/shreddingData.py
+++ CSE/Unit3/Notes/FileWriting/shreddingData.py
@@ -1,19 +1,20 @@
-filename="TestData.cvs"
+filename="./FileWriting/testdata.csv"
 classes=["Advanced","Engineer","Collision","Automotive Service","Construction","CISCO","Software","Culinary","Diesel","Electrical","Graphic","Health","HVAC","Precision","Public","Radio","Vet","Welding"]
-Advanced,Engineer,Collision,AutomotiveService,Construction,CISCO,Software,Culinary,Diesel,Electrical,Graphic,Health,HVAC,Precision,Public,Radio,Vet,Welding=[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]#with open(fileYouAreOpening,mode) as variable
+#advanced,engineer,collision,automotiveService,construction,cisco,software,culinary,diesel,electrical,graphic,health,hvac,precision,public,radio,vet,welding = [],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]
+listOfList=[]
+
+for i in classes:
+    listOfList.append([])
+
 with open(filename,"r") as file:
-    #readlines returns a list
-    listOflines= file.readlines()
+    listOfLines = file.readlines()
 
-for row in listOflines:
-    #rstrip() removes the \n on the end of the row
-    #split() splits the row based on a character
-    if classes[0] in row:
-        Advanced.append(row)
-    #rowSplitter = row.rstrip().split("\t",7)
-    #print(row)
-#print(listOflines)
+for row in listOfLines:
+    for i in range(len(classes)):
+        if classes[i] in row:
+            listOfList[i].append(row)
 
-with open("adcanced.csv","w") as fileToWrite:
-    for row in Advanced:
-        print(row)
+for i in range(len(classes)):
+    with open(f"./Sorted/{classes[i]}.csv","w") as fileToWrite:
+        for row in listOfList[i]:
+            fileToWrite.write(row)
