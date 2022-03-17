56f948b2c78ac93c2f7b36c955352d4ebbaa1474
younglings <christian.gates@evsck12.com>
2022-03-17 10:02:40 -0500

    hello

--- CSP/Unit3/Notes/Harry/mainHarry.py
+++ CSP/Unit3/Notes/Harry/mainHarry.py
@@ -1,4 +1,5 @@
 import glob
+import matplotlib.pyplot as plt
 
 def makeFiles(name):
     #relative file path => the path from your file to another file
@@ -7,7 +8,6 @@ def makeFiles(name):
         
     #print(file)
 
-    print("original length of file: ",len(file))
 
     reducedFile=[]
     for line in file:
@@ -19,7 +19,6 @@ def makeFiles(name):
     with open(f"out/{name}","w+",encoding="utf-8") as f:
         for line in reducedFile:
             f.write(line)
-    print("reduced length of file: ",len(reducedFile))
 
 #print which books are present in directory
 books = (glob.glob("data/*.txt"))       #* stands for wildcard
@@ -27,8 +26,39 @@ books = (glob.glob("data/*.txt"))       #* stands for wildcard
 bookList=[]
 for b in books:
     bookList.append(b[5:])
-print(bookList)
 
 for i in range(len(bookList)):
     makeFiles(bookList[i])
-    print()
+    
+#Going to find the page count
+cleanBookList=glob.glob("out/*.txt")
+bookList=[]
+for b in cleanBookList:
+    bookList.append(b[4:])
+
+def pageNumber(FILENAME):
+    with open(f"out/{FILENAME}","r",encoding="utf-8") as f:
+        file = f.readlines()
+        lastLine=(file[-1])
+        #find the numbers in the lastLine
+        number=""
+        for i in range(len(file)):
+            for letter in lastLine:
+                if letter.isdigit():
+                    number+=letter
+            if number !="":
+                return int(number)
+            lastLine=file[-i]
+            
+            
+pageCount=[]
+#Loop through the books, find the number of pages in each book,
+#       and graph this in a graph
+for i in range(len(bookList)):
+    pageCount.append(pageNumber(bookList[i]))
+titles=[1,2,3,4,5,6,7]
+plt.bar(titles,pageCount)
+plt.title("Num of Pages in each HP Book")
+plt.xlabel("Book #")
+plt.ylabel("Page Count")
+plt.show()
