17a7c27686bc7db057403013c40d686093c728cd
younglings <christian.gates@evsck12.com>
2022-03-28 10:05:17 -0500

    3/28

--- CSP/Unit2/HW/GUI/Sudoku/SudokuChecker.py
+++ CSP/Unit2/HW/GUI/Sudoku/SudokuChecker.py
@@ -1,133 +1,63 @@
 import os,time,math
 #       i know i forgot to comment,comment,comment -5
-    
-#Opens the files to add the content into a list while removing unnecessary elements
-def openFile(puzzle):
-    global puzzleFromFile
-    with open(puzzle,"r+") as p:
-                puzzleFromFile=p.readlines()
-                index=0
-                for i in puzzleFromFile:
-                    puzzleFromFile[index]=i.replace("\n","")
-                    index+=1
-                index=0
-                for i in puzzleFromFile:
-                    puzzleFromFile[index] = []
-                    for j in i:
-                        puzzleFromFile[index].append(j)
-                    index+=1
-                p.close
 
-#checking to see if there is a issue with the horizontal part
-def horizontalChecker():
-    rowspot=1
-    problems=[]
-    problemType=["horizontal",'row','column']
-    for row in puzzleFromFile:
-        nums=[]
-        col=1
-        for i in row:
-            if i in nums:
-                problems.append(str(col))
-                problems.append(str(rowspot))
-                #os.system.exit("There was a horizontal error ")    would stop the program
-            else:  
-                nums.append(i)
-            col+=1
-        rowspot+=1
-    return problems,problemType
+class Checker:
+    #Opens the files to add the content into a list while removing unnecessary elements
+    def openFile(self,puzzle):
+        global puzzleFromFile
+        with open(puzzle,"r+") as p:
+            puzzleFromFile=p.readlines()
+            index=0
+            for i in puzzleFromFile:
+                puzzleFromFile[index]=i.replace("\n","")
+                if len(i) >= 9:
+                    print("There is an error in your Sudoku File please fix it then try again")
+                    return False
+                index+=1
+            index=0
+            for i in puzzleFromFile:
+                puzzleFromFile[index] = []
+                for j in i:
+                    if not(i.isnumeric) and i != "":
+                        print("There is an error in your Sudoku File please fix it then try again")
+                        return False
+                    puzzleFromFile[index].append(j)
+                index+=1
+            p.close
+            return True,puzzleFromFile
 
-def verticalChecker():
-    problems=[]
-    problemType=["vertical",'column','row']
-    for col in range(len(puzzleFromFile)):
-        nums=[]
-        for i in range(len(puzzleFromFile)):
-            if puzzleFromFile[i][col] in nums:
-                problems.append(str(i+1))
-                problems.append(str(col+1))
-            else:  
-                nums.append(puzzleFromFile[i][col])
-    return problems,problemType
+    #checking to see if there is a issue with the horizontal part
+    def horizontalChecker(self):
+        for row in puzzleFromFile:
+            nums=[]
+            for i in row:
+                if i in nums:
+                    print()
+                    #os.system.exit("There was a horizontal error ")    would stop the program
+                else:  
+                    nums.append(i)
 
-def sectionChecker():
-    problems=[]
-    problemType=["section",'row','column']
-    cols=0
-    rows=0
-    for sec in range(len(puzzleFromFile)):
-        nums = []
-        for i in range(math.floor(len(puzzleFromFile)/3)): 
-            for j in range(math.floor(len(puzzleFromFile)/3)):
-                if puzzleFromFile[i+cols][j+rows] in nums:
-                    problems.append(str(sec+1))
-                    problems.append(str(j+rows+1))
-                    problems.append(str(i+cols+1))
-                else:
-                    nums.append(puzzleFromFile[i+cols][j+rows])
-        rows+=3
-        if (sec+1)%3==0:
-            cols+=3
-            rows=0
-    return problems,problemType
+    def verticalChecker(self):
+        for col in range(len(puzzleFromFile)):
+            nums=[]
+            for i in range(len(puzzleFromFile)):
+                if puzzleFromFile[i][col] in nums:
+                    print()
+                else:  
+                    nums.append(puzzleFromFile[i][col])
 
-
-#used to change the list from the checkers to print out a string in the problem row
-def listToString(problems,problemType):
-    if len(problems) > 0: 
-        word=""
-        for i in problems:
-            word+=i
-            if len(word)-1>=0:
-                word+=","
-        word = word[:-1]
-        
-        if problemType[0]!="section":
-            problem=str(len(problems)/2)
-            print(f"The puzzle has {problem[0]} {problemType[0]} problem(s) in {problemType[1]}/{problemType[2]} {word}")
-            print(f"First number is the {problemType[1]} and second is the {problemType[2]}")
-            print()
-        else: 
-            problem=str(len(problems)/3)
-            print(f"The puzzle has {problem[0]} {problemType[0]} problem(s) in {problemType[0]}/{problemType[1]}/{problemType[2]} {word}")
-            print(f"First number is the {problemType[0]}, second is the {problemType[1]}, and the third  is the {problemType[2]}")
-            print()
-    else:
-        print(f"The puzzle's {problemType[0]} has no problems")
-        print()
-    
-
-
-ui = input("Would you like to 'solve' or 'check' a Sudoku Puzzle ").lower()
-while ui != "solve" and ui != "check":
-    ui = input("Would you like to 'solve' or 'check' a Sudoku Puzzle ").lower()
-if ui == "check":    
-    ui = input("Would you like to 'enter' your puzzle or 'import' your puzzle ").lower()
-    while ui != "enter" and ui != "import":
-        ui = input("Would you like to 'enter' your puzzle or 'import' your puzzle ").lower()
-    while ui == "import":
-#       https://stackoverflow.com/questions/11968976/list-files-only-in-the-current-directory      used to print the files in the directory for user friendlyness and for bad spelling
-        print(os.listdir(os.curdir))
-        print("Note please use txt files")
-        puzzle = input("Which file would you like to import ")
-        if not(os.path.exists(puzzle)):
-            print("This file does not exist please see if you entered the name wrong or if the file is in the current directory")
-            print("Note did you forget .txt")
-            print()
-            time.sleep(1)
-            continue
-        else:
-            openFile(puzzle)
-            os.system('cls')
-            problems,problemType = horizontalChecker()
-            listToString(problems,problemType)
-            problems,problemType = verticalChecker()
-            listToString(problems,problemType)
-            problems,problemType = sectionChecker()
-            listToString(problems,problemType)
-            time.sleep(5)
-    while ui == "enter":
-        print("This is still currently being worked on")
-        break
-else:
-    print("This is still currently being worked on")
+    def sectionChecker(self):
+        cols=0
+        rows=0
+        for sec in range(len(puzzleFromFile)):
+            nums = []
+            for i in range(math.floor(len(puzzleFromFile)/3)): 
+                for j in range(math.floor(len(puzzleFromFile)/3)):
+                    if puzzleFromFile[i+cols][j+rows] in nums:
+                        print
+                    else:
+                        nums.append(puzzleFromFile[i+cols][j+rows])
+            rows+=3
+            if (sec+1)%3==0:
+                cols+=3
+                rows=0
--- CSP/Unit2/HW/GUI/Sudoku/SudokuGUI.py
+++ CSP/Unit2/HW/GUI/Sudoku/SudokuGUI.py
@@ -16,93 +16,96 @@ Bonus opportunity:  Add to the GUI the ability for the user to create a puzzle o
 If you solve using random number generation, will work, but more strategy the better.  You will need to output the finished puzzle to user."""
 
 from tkinter import *
-#import SudokuChecker as Checker
+import SudokuChecker as Checker
 import os,time,random
 
 root = Tk()
 root.title("Sudoku")
 root.wm_geometry("400x400")
 
-frames=[]
-curframe=0
-def nextFrame(r):
-    info = something.get()
-    info2 = something2.get()
-    if info != "" and info2 != "":
-        if info != "something" and info2 != "something else":
-            curframe+=1
-            frames[curframe].tkraise()
+
+while True:
+    root.update()
+
+'''frames=[]
+    curframe=0
+    def nextFrame(r):
+        info = something.get()
+        info2 = something2.get()
+        if info != "" and info2 != "":
+            if info != "something" and info2 != "something else":
+                curframe+=1
+                frames[curframe].tkraise()
+            else:
+                info.set("Please enter the correct information to move on")
         else:
-            info.set("Please enter the correct information to move on")
-    else:
-        info.set("Please do not leave this field blank")
-root.bind('<Return>', lambda r :nextFrame(r))
+            info.set("Please do not leave this field blank")
+    root.bind('<Return>', lambda r :nextFrame(r))
 
-#https://www.tutorialspoint.com/how-to-bind-the-escape-key-to-close-a-window-in-tkinter     makes esc close the window
-def closeWindow(e):
-    root.destroy()
-root.bind('<Escape>', lambda e: closeWindow(e))
+    #https://www.tutorialspoint.com/how-to-bind-the-escape-key-to-close-a-window-in-tkinter     makes esc close the window
+    def closeWindow(e):
+        root.destroy()
+    root.bind('<Escape>', lambda e: closeWindow(e))
 
-def notFinished():
-    print("This section is still being worked on please come back later when it is finished")
-    return True
+    def notFinished():
+        print("This section is still being worked on please come back later when it is finished")
+        return True
 
-def downloadPuzzles():
-    puzzlesName=["Puzzle1.txt","Puzzle2.txt","Puzzle3.txt"]
-    puzzles=[[[' ', '5', ' ', '1', ' ', ' ', ' ', ' ', ' '], ['2', ' ', '4', ' ', ' ', ' ', ' ', '9', '3'], ['7', '2', '1', ' ', '3', '8', '6', '4', ' '], ['4', '3', ' ', ' ', '5', '7', '9', '8', '1'], [' ', ' ', ' ', ' ', '6', '1', ' ', ' ', '2'], [' ', ' ', ' ', ' ', ' ', '4', ' ', ' ', '9'], ['6', '4', ' ', '8', ' ', '2', ' ', ' ', ' ']],
-    [[' ', ' ', '6', '5', '1', ' ', ' ', ' ', '8'], ['7', ' ', '3', '8', ' ', ' ', '6', '9', '1'], ['2', ' ', ' ', ' ', '3', ' ', '5', '4', '7'], [' ', ' ', '1', '7', ' ', ' ', ' ', '8', ' '], ['6', ' ', ' ', '3', ' ', '1', '7', '5', ' '], ['8', ' ', '7', '4', '5', ' ', '3', '1', ' '], [' ', '8', '5', '6', ' ', ' ', '9', ' ', ' '], ['9', ' ', ' ', ' ', ' ', '5', ' ', '7', ' '], [' ', '7', '4', ' ', ' ', ' ', ' ', ' ', ' ']],
-    [[' ', '8', '9', ' ', '6', ' ', ' ', ' ', ' '], [' ', ' ', ' ', '5', ' ', ' ', ' ', ' ', '8'], ['6', '1', ' ', ' ', ' ', ' ', '9', '3', ' '], [' ', ' ', ' ', '4', '3', ' ', ' ', '7', '9'], ['1', ' ', ' ', ' ', '9', ' ', ' ', ' ', ' '], [' ', ' ', '4', '6', ' ', ' ', '8', '5', ' '], ['3', ' ', ' ', ' ', '5', '7', ' ', ' ', ' '], [' ', '9', '7', ' ', '4', '6', '5', '2', '3'], ['5', '4', '1', ' ', '2', '8', ' ', '9', '6']]]
-    print(puzzles[0])
-    print(puzzles[1])
-    puzzle1=[puzzles[0]]
-    puzzle2=[]
-    puzzle3=[]
-    for i in puzzlesName:
-        puzzleNum=0
-        with open(i,"w+") as p:
-            for i in range(len(puzzles[puzzleNum])):
-                p.writelines(puzzles[puzzleNum][i])
-                p.write("\n")
-        puzzleNum+=1
-        print("is this working")
-downloadPuzzles()
-    
-if  not(os.path.exists('Puzzle1.txt')):
-    info.set("Would you like to 'download' the 3 demo Sudoku files, 'import' your own, or 'enter' one.")
-    info = something.get()
-    while info != "download" or info != "import" or info != "enter":
+    def downloadPuzzles():
+        puzzlesName=["Puzzle1.txt","Puzzle2.txt","Puzzle3.txt"]
+        puzzles=[[[' ', '5', ' ', '1', ' ', ' ', ' ', ' ', ' '], ['2', ' ', '4', ' ', ' ', ' ', ' ', '9', '3'], ['7', '2', '1', ' ', '3', '8', '6', '4', ' '], ['4', '3', ' ', ' ', '5', '7', '9', '8', '1'], [' ', ' ', ' ', ' ', '6', '1', ' ', ' ', '2'], [' ', ' ', ' ', ' ', ' ', '4', ' ', ' ', '9'], ['6', '4', ' ', '8', ' ', '2', ' ', ' ', ' ']],
+        [[' ', ' ', '6', '5', '1', ' ', ' ', ' ', '8'], ['7', ' ', '3', '8', ' ', ' ', '6', '9', '1'], ['2', ' ', ' ', ' ', '3', ' ', '5', '4', '7'], [' ', ' ', '1', '7', ' ', ' ', ' ', '8', ' '], ['6', ' ', ' ', '3', ' ', '1', '7', '5', ' '], ['8', ' ', '7', '4', '5', ' ', '3', '1', ' '], [' ', '8', '5', '6', ' ', ' ', '9', ' ', ' '], ['9', ' ', ' ', ' ', ' ', '5', ' ', '7', ' '], [' ', '7', '4', ' ', ' ', ' ', ' ', ' ', ' ']],
+        [[' ', '8', '9', ' ', '6', ' ', ' ', ' ', ' '], [' ', ' ', ' ', '5', ' ', ' ', ' ', ' ', '8'], ['6', '1', ' ', ' ', ' ', ' ', '9', '3', ' '], [' ', ' ', ' ', '4', '3', ' ', ' ', '7', '9'], ['1', ' ', ' ', ' ', '9', ' ', ' ', ' ', ' '], [' ', ' ', '4', '6', ' ', ' ', '8', '5', ' '], ['3', ' ', ' ', ' ', '5', '7', ' ', ' ', ' '], [' ', '9', '7', ' ', '4', '6', '5', '2', '3'], ['5', '4', '1', ' ', '2', '8', ' ', '9', '6']]]
+        print(puzzles[0])
+        print(puzzles[1])
+        puzzle1=[puzzles[0]]
+        puzzle2=[]
+        puzzle3=[]
+        for i in puzzlesName:
+            puzzleNum=0
+            with open(i,"w+") as p:
+                for i in range(len(puzzles[puzzleNum])):
+                    p.writelines(puzzles[puzzleNum][i])
+                    p.write("\n")
+            puzzleNum+=1
+            print("is this working")
+    downloadPuzzles()
+        
+    if  not(os.path.exists('Puzzle1.txt')):
         info.set("Would you like to 'download' the 3 demo Sudoku files, 'import' your own, or 'enter' one.")
         info = something.get()
-    if info == "download":
-        if not(notFinished()):
-                downloadPuzzles()
-                nextFrame()
-    elif info == "import":
-        if not(notFinished()):
-                puzzleEntry = Entry(curframe,bd=3,text="Which file would you like to import: ")
-                nextFrame()
-    else:
-        if not(notFinished()):
-            puzzleEntry = Entry(curframe,bd=3,text="Please enter your puzzle (for blanks please enter ., ,#): ")
-            enterPuzzle()
+        while info != "download" or info != "import" or info != "enter":
+            info.set("Would you like to 'download' the 3 demo Sudoku files, 'import' your own, or 'enter' one.")
+            info = something.get()
+        if info == "download":
+            if not(notFinished()):
+                    downloadPuzzles()
+                    nextFrame()
+        elif info == "import":
+            if not(notFinished()):
+                    puzzleEntry = Entry(curframe,bd=3,text="Which file would you like to import: ")
+                    nextFrame()
+        else:
+            if not(notFinished()):
+                puzzleEntry = Entry(curframe,bd=3,text="Please enter your puzzle (for blanks please enter ., ,#): ")
+                enterPuzzle()
 
-puzzleFromFile=[]
-def openFile(puzzle):
-    global puzzleFromFile
-    with open(puzzle,"r+") as p:
-        puzzleFromFile=p.readlines()
-        index=0
-        for i in puzzleFromFile:
-            puzzleFromFile[index]=i.replace("\n","")
-            index+=1
-        index=0
-        for i in puzzleFromFile:
-            puzzleFromFile[index] = []
-            for j in i:
-                puzzleFromFile[index].append(j)
-            index+=1
-        p.close
+    puzzleFromFile=[]
+    def openFile(puzzle):
+        global puzzleFromFile
+        with open(puzzle,"r+") as p:
+            puzzleFromFile=p.readlines()
+            index=0
+            for i in puzzleFromFile:
+                puzzleFromFile[index]=i.replace("\n","")
+                index+=1
+            index=0
+            for i in puzzleFromFile:
+                puzzleFromFile[index] = []
+                for j in i:
+                    puzzleFromFile[index].append(j)
+                index+=1
+            p.close
 
-openFile(input())
-print(puzzleFromFile)
-root.mainloop()
+    openFile(input())
+    print(puzzleFromFile)'''
