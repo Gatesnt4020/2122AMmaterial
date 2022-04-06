53c628e0751ee1103bc5dd73db8475b48015edbf
younglings <christian.gates@evsck12.com>
2022-04-06 10:08:53 -0500

    get done with bellringer and ws

--- CSP/Unit3/Bellringer/Weather/main.py
+++ CSP/Unit3/Bellringer/Weather/main.py
@@ -1,5 +1,37 @@
 import pandas as pd
 
-df = pd.read_table("EVV Weather Obs.txt")
+def makeFile():
+    with open('EVV Weather Obs.txt','r+') as file:
+        f=file.readlines()
+        for i in range(len(f)):
+            temp=""
+            lastChr=""
+            for j in range(len(f[i])):
+                if lastChr != " " or f[i][j] != " ":
+                    if f[i][j] == " ":
+                        if lastChr.isnumeric() or lastChr == "S":
+                            temp+=","
+                        else:
+                            if i!=0:
+                                temp+=" "
+                            else:
+                                temp+=","
+                    else:
+                        temp+=f[i][j]
+                lastChr=f[i][j]
+            temp=temp[:-2]
+            temp+="\n"    
+            f[i] = temp
+            f[i] = f[i]
+            f[i] = f[i].replace('-9999',"")
+        file.close()
+    with open("new EVV Weather Obs.csv","w+") as file:
+        for line in f:
+            file.write(line)
+        file.close()
 
-unq= df['TOBS']
+#makeFile()
+
+df = pd.read_csv("new EVV Weather Obs.csv",header=0)
+un = df['TOBS'].unique()
+print(un)
